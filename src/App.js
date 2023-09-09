5440f6343f174d7b51676087a0be64e81a424868
Ragul-9862 <121421581+Ragul-9862@users.noreply.github.com>
2023-09-09 15:47:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Postpage from './Pages/Postpage';
+
+function App() {
+  return (
+  <React.StrictMode>
+  <Postpage/>
+  </React.StrictMode>
+  );
+}
+
+export default App;
